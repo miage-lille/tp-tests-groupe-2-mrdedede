03605e12b0958d4488c77acbdb1471a5c4b2afb3
mrdedede <andore.filho2000@gmail.com>
2026-02-14 19:29:26 +0100

    feqt: tests e2e

---
+++ src/api.e2e.test.ts
@@ -0,0 +1,117 @@
+import supertest from "supertest";
+import { TestServerFixture } from "./tests/fixtures";
+
+describe('Webinar Routes E2E', () => {
+  let fixture: TestServerFixture;
+
+  beforeAll(async () => {
+    fixture = new TestServerFixture();
+    await fixture.init();
+  });
+
+  beforeEach(async () => {
+    await fixture.reset();
+  });
+
+  afterAll(async () => {
+    await fixture.stop();
+  });
+  describe('Scenario: Happy Path', async () => {
+    it('should update webinar seats', async () => {
+      // ARRANGE
+      const prisma = fixture.getPrismaClient();
+      const server = fixture.getServer();
+
+      const webinar = await prisma.webinar.create({
+        data: {
+          id: 'test-webinar',
+          title: 'Webinar Test',
+          seats: 10,
+          startDate: new Date(),
+          endDate: new Date(),
+          organizerId: 'test-user',
+        },
+      });
+
+      // ACT
+      const response = await supertest(server)
+        .post(`/webinars/${webinar.id}/seats`)
+        .send({ seats: '30' })
+        .expect(200);
+
+      // ASSERT
+      expect(response.body).toEqual({ message: 'Seats updated' });
+
+      const updatedWebinar = await prisma.webinar.findUnique({
+        where: { id: webinar.id },
+      });
+      expect(updatedWebinar?.seats).toBe(30);
+    });
+  });
+  
+  describe('Scenario: Update other user\'s webinar', async () => {
+    it('should throw a 401 error', async () => {
+      // ARRANGE
+      const prisma = fixture.getPrismaClient();
+      const server = fixture.getServer();
+
+      const webinar = await prisma.webinar.create({
+        data: {
+          id: 'test-webinar',
+          title: 'Webinar Test',
+          seats: 10,
+          startDate: new Date(),
+          endDate: new Date(),
+          organizerId: 'test-user-d',
+        },
+      });
+
+      // ACT
+      const response = await supertest(server)
+        .post(`/webinars/${webinar.id}/seats`)
+        .send({ seats: '30' })
+        .expect(401);
+
+      // ASSERT
+      expect(response.body).toEqual({ message: 'User is not allowed to update this webinar' });
+
+      const updatedWebinar = await prisma.webinar.findUnique({
+        where: { id: webinar.id },
+      });
+      expect(updatedWebinar?.seats).toBe(10);
+    });
+  });
+
+  describe('Scenario: Update unexistant webinar', async () => {
+    it('should throw a 404 error', async () => {
+      // ARRANGE
+      const prisma = fixture.getPrismaClient();
+      const server = fixture.getServer();
+
+      const webinar = await prisma.webinar.create({
+        data: {
+          id: 'test-webinar',
+          title: 'Webinar Test',
+          seats: 10,
+          startDate: new Date(),
+          endDate: new Date(),
+          organizerId: 'test-user-d',
+        },
+      });
+
+      // ACT
+      const response = await supertest(server)
+        .post(`/webinars/test-webinar-b/seats`)
+        .send({ seats: '30' })
+        .expect(404);
+
+      // ASSERT
+      expect(response.body).toEqual({ message: 'Webinar not found' });
+
+      const updatedWebinar = await prisma.webinar.findUnique({
+        where: { id: webinar.id },
+      });
+      expect(updatedWebinar?.seats).toBe(10);
+    });
+  });
+});
